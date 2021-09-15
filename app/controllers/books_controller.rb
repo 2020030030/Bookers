59c98b4c687c768ac63161543b3e78db71acd995
2020030030 <nagomicunshang@gmail.com>
2021-09-15 02:11:14 +0000

    途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+
+
+  def index
+    @book = Book.new
+    @bookers = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+   if @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path
+    flash[:notice] = "Book was successfully updated."
+   else
+     render :edit
+   end
+  end
+
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully updated."
+  end
+
+
+  private
+
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+
+  end
+
+end
